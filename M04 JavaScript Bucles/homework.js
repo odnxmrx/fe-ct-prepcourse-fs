e423e0acf6e4ad6013ddca5ba5256607fa9a21c3
Armando Pi <armandopinedag@outlook.com>
2023-07-31 19:45:20 -0700

    prueba de copia, m4 modificado

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -13,7 +13,7 @@ function mayoriaDeEdad(edad) {
    // Si tiene 18 años ó más debe retornar el string: "Allowed".
    // Caso contrario: "Not allowed".
    // Tu código:
-   if(edad >= 18) {
+   if(edad > 17) {
       return "Allowed";
    } else {
       return "Not allowed";
